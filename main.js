db458508322d9070de840dff9db9e13b3a0973d7
DestinyJoyner <destinyjoyner@pursuit.org>
2022-09-16 12:53:45 -0400

    dropdown population and display of personInfo success, onto shoutout

--- main.js
+++ main.js
@@ -18,12 +18,28 @@ const fetchInfo = () => {
             // Variables for 'info' div
             const personInfo = document.createElement(`article`)
             personInfo.classList.add('hidden')
+            personInfo.id = `${name}`
             personInfo.innerHTML = `
             <h4>${name}</h4>
             <p>Age: ${age}</p>
             <p>Eye Color: ${eye_color}</p>
             <p>Hair Color: ${hair_color}</p>`
 
+            document.getElementById(`info`).append(personInfo)
+
         })
     })
 }
+// Call fetch function
+// fetchInfo()
+// ADD EVENT LISTENER TO DROPDOWN 
+dropDown.addEventListener(`change`, (e) => {
+    // conditional to remove previous person's info before displaying the next selection
+    if(document.querySelector(`.show`)){
+        document.querySelector(`.show`).classList.toggle('hidden')
+        document.querySelector(`.show`).classList.remove('show')
+}
+    document.getElementById(`${e.target.value}`).classList.toggle(`hidden`)
+    document.getElementById(`${e.target.value}`).classList.add(`show`)
+
+})
